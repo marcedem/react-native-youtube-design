67e6583c72ff4770e23281b6edc0234e3b6bd18c
marcedem <marcsea2000@gmail.com>
2018-01-24 11:47:29 +0100

    adding youtube logo

--- app/App.js
+++ app/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        
+        <View style={styles.navBar}>
+          <Image source={require('./img/yt_logo_rgb_light.png')} style={styles.youtubeLogo}/>
+        </View>
+        <View style={styles.rightNav}>
+
+        </View>
       </View>
     );
   }
@@ -14,8 +20,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  navBar: {
+    height: 55,
+    backgroundColor: '#FFF',
+    elevation: 3,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  youtubeLogo:{
+    width: 98,
+    height: 22,
+  }
 });
